6a22f2589581682b3cfc7791020fa26bfceb0463
ericAZ99 <eazankpo@vippinterstis.com>
2024-01-04 10:18:21 +0100

    ajout de "OnInit" pour initialiser un action dans la vue

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  ngOnInit(): void {
+    // Ajustez ceci si nécessaire
+    this.loginMethod();
+  }
+
+
+  loginMethod() {
+    let login = document.getElementById('login');
+    let loginImage = document.getElementById('loginImage');
+
+    if (login && loginImage) {
+      login.style.transform = "scale(0)";
+      loginImage.style.transform = "scale(.1)";
+
+      setTimeout(() => {
+        if (loginImage) {
+          loginImage.style.transition = "2s";
+          loginImage.style.transform = "scale(1)";
+        }
+        setTimeout(() => {
+          if (login) {
+            login.style.transition = "2s";
+            login.style.transform = "scale(1)";
+          }
+        }, 1000);
+      },100);
+    }
+  }
 }
